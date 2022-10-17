d27c19825c0d08012a6f77f37f0f0e855eda2a42
IsaacKunmi <kunmiolamiju@gmail.com>
2022-10-17 12:38:13 -0700

    ALx printf team project

---
+++ README.md
@@ -0,0 +1,1 @@
+Create a printf function for ALX project as a team project
---
+++ print.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+
+int _printf(const char *format, ... )
+{
+
+	va_list start;
+
+	va_start(start, format)
+}
